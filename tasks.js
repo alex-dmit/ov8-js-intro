c3508dd7023e657bbaa602e7f53603b1e2f11783
Alex Dmit <alex.golostos@gmail.com>
2023-04-18 22:02:17 +0300

    Elementary tasks

--- tasks.js
+++ tasks.js
@@ -39,32 +39,70 @@
 // })
 // console.log(mult);
 
-const people = [5, 15, 24, 12, 70, 54, 18]
-const adults = []
-const children = [] // kids
-// let childrenIndex = 0, adultsIndex = 0
-// for (let index = 0; index < people.length; index++) {
-//     if (people[index] > 17) {
-//         adults[adultsIndex] = people[index]
-//         adultsIndex++
-//     } else {
-//         children[childrenIndex] = people[index]
-//         childrenIndex++
-//     }
+// const people = [5, 15, 24, 12, 70, 54, 18]
+// const adults = []
+// const children = [] // kids
+// // let childrenIndex = 0, adultsIndex = 0
+// // for (let index = 0; index < people.length; index++) {
+// //     if (people[index] > 17) {
+// //         adults[adultsIndex] = people[index]
+// //         adultsIndex++
+// //     } else {
+// //         children[childrenIndex] = people[index]
+// //         childrenIndex++
+// //     }
+// // }
+// // console.log('Adults:');
+// // console.log(adults);
+// // console.log('Children:');
+// // console.log(children);
+
+// // For of loop + push
+// for (let element of people) {
+//     if (element > 17) adults.push(element)
+//     else children.push(element)
 // }
 // console.log('Adults:');
 // console.log(adults);
 // console.log('Children:');
 // console.log(children);
 
-// For of loop + push
-for (let element of people) {
-    if (element > 17) adults.push(element)
-    else children.push(element)
-}
-console.log('Adults:');
-console.log(adults);
-console.log('Children:');
-console.log(children);
-
 // DZ: foreach + push
+// find an array with 12 primal numbers
+// 1 2 3 5 7 11 13 ...
+// 7 % 6
+// let n = 20
+// let primal = []
+// if (n > 0) {
+//     let number = 1
+//     while (primal.length < n) {
+//         let isPrimal = true
+//         for (let del = 2; del < number; del++) {
+//             if (number % del == 0) isPrimal = false
+//         }
+//         if (isPrimal) primal.push(number)
+//         number++
+//     }
+// }
+// console.log(primal);
+
+// let fibo = [0, 1]
+// let n = 15
+// if (n > 2) {
+//     while (fibo.length < n) {
+//         fibo.push(fibo[fibo.length - 1] + fibo[fibo.length - 2])
+//     }
+// }
+// console.log(fibo);
+
+// factorial => n!
+// 8! = 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8
+
+let n = 10
+if (n > 0) {
+    let mult = 1
+    for (let i = 1; i <= n; i++) {
+        mult = mult * i
+    }
+    console.log(mult);
+}
